abdb94a341d04c1c12aad839f32f06b46e3566c6
Nagarchunan P <nagarchunan@gmail.com>
2024-08-26 08:37:34 +0530

    Main Navigation System

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
+// Show dropdown on hover
 $(document).ready(function () {
-    // Show dropdown on hover
     $(".nav-item.dropdown").hover(
         function () {
             $(this)
@@ -16,9 +16,31 @@ $(document).ready(function () {
                 .slideUp(150);
         }
     );
-
-    // Prevent the dropdown from closing when clicking inside it
     $(".dropdown-menu").on("click", function (e) {
         e.stopPropagation();
     });
 });
+
+
+
+// Main Navigation System 
+$(document).ready(function() {
+    $('a.nav-link').click(function(event) {
+      event.preventDefault();
+
+      // Hide all sections
+      $('.section').addClass('d-none');
+
+      // Show the selected section
+      const target = $(this).attr('href');
+      $(target).removeClass('d-none');
+
+      // Update active link
+      $('a.nav-link').removeClass('active');
+      $(this).addClass('active');
+
+      // Toggle the visibility of the <i> tags
+      $('a.nav-link i').addClass('d-none'); // Hide all <i> tags
+      $(this).find('i').removeClass('d-none'); // Show the <i> tag of the active link
+    });
+  });
