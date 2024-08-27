c576d558dbfd9325d2000a5cc6561e4a50a3f85e
Nagarchunan P <nagarchunan@gmail.com>
2024-08-27 19:09:45 +0530

    Homepage Responsive Layout Updated

--- js/main.js
+++ js/main.js
@@ -22,26 +22,22 @@ $(document).ready(function () {
 });
 
 
+/* 
+$(document).ready(function () {
+    $('a.nav-link').click(function (event) {
+        event.preventDefault();
 
-$(document).ready(function() {
-    $('a.nav-link').click(function(event) {
-      event.preventDefault();
-  
-      // Hide all sections
-      $('.section').addClass('d-none');
-  
-      // Show the selected section
-      const target = $(this).attr('href');
-      console.log('Target Section:', target); // Debugging line
-      $(target).removeClass('d-none');
-  
-      // Update active link
-      $('a.nav-link').removeClass('active');
-      $(this).addClass('active');
-  
-      // Toggle the visibility of the <i> tags
-      $('a.nav-link i').addClass('d-none'); // Hide all <i> tags
-      $(this).find('i').removeClass('d-none'); // Show the <i> tag of the active link
+        $('.section').addClass('d-none');
+
+        const target = $(this).attr('href');
+        console.log('Target Section:', target); 
+        $(target).removeClass('d-none');
+
+        $('a.nav-link').removeClass('active');
+        $(this).addClass('active');
+
+        $('a.nav-link i').addClass('d-none');
+        $(this).find('i').removeClass('d-none');
     });
-  });
-  
+});
+ */
